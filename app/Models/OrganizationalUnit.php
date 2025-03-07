f972bd8b9fe2c290d592d5d2d63d0f84a07a965b
sadeghbiglar <sbmail555@gmail.com>
2025-03-07 13:36:01 +0330

    create county-province-units models

---
+++ app/Models/OrganizationalUnit.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrganizationalUnit extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'province_id',
+        'county_id',
+        'parent_id'
+    ];
+
+    // اگر این واحد در سطح استان باشد
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    // اگر این واحد مربوط به شهرستان باشد
+    public function county()
+    {
+        return $this->belongsTo(County::class);
+    }
+
+    // رابطه برای ساختار سلسله مراتب: والد
+    public function parent()
+    {
+        return $this->belongsTo(OrganizationalUnit::class, 'parent_id');
+    }
+
+    // رابطه برای ساختار سلسله مراتب: فرزندان
+    public function children()
+    {
+        return $this->hasMany(OrganizationalUnit::class, 'parent_id');
+    }
+}
